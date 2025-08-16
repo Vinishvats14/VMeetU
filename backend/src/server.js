f9a44f07c95f7188c66071d5804ec57324ff4507
vinishvats14 <vvinishvats@gmail.com>
2025-08-16 13:55:41 +0530

    update env.example with CLIENT_URL

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true, // allow frontend to send cookies
   })
 );
