cc1b4647971365aab5385c183cd9b88e7906605a
vinishvats14 <vvinishvats@gmail.com>
2025-08-16 17:01:58 +0530

    conole checking

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:1410/api" : import.meta.env.VITE_API_BASE_URL;
+console.log("Axios BASE_URL:", BASE_URL); // Debug log
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
